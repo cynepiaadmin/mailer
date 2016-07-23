4b49f9e9516c7530223ed62173c0ba8c653622d0
cynepiaadmin <adminsupport@cynepia.com>
2016-07-23 16:57:10 +0530

    Add files via upload

--- marrow/mailer/transport/ses.py
+++ marrow/mailer/transport/ses.py
@@ -1,9 +1,8 @@
 # encoding: utf-8
 
+# TODO: Port: https://github.com/pankratiev/python-amazon-ses-api/blob/master/amazon_ses.py
 try:
-    import boto.ses 
-    from boto.ses import SESConnection
-
+    import boto.ses
 except ImportError:
     raise ImportError("You must install the boto package to deliver mail via Amazon SES.")
 
@@ -15,26 +14,27 @@ log = __import__('logging').getLogger(__name__)
 
 
 class AmazonTransport(object): # pragma: no cover
-    __slots__ = ('ephemeral', 'config', 'region', 'connection')
+    __slots__ = ('ephemeral', 'id', 'key', 'host', 'region', 'connection')
     
     def __init__(self, config):
-        # Give our configuration aliases their proper names.
-        config['aws_access_key_id'] = config.pop('id')
-        config['aws_secret_access_key'] = config.pop('key')
-        
-        self.region = config.pop('region', "us-east-1")
-        config.pop('use') #boto throws an error if we leave this in the next line
-        self.config = config  # All other configuration directives are passed to connect_to_region.
+        self.id = config.get('id')
+        self.key = config.get('key')
+        self.host = config.get('host', "us-west-2")
         self.connection = None
     
     def startup(self):
-        self.connection = boto.ses.connect_to_region(self.region, **self.config)
+        self.connection = boto.ses.connect_to_region(
+                    self.host,
+                    aws_access_key_id=self.id,
+                    aws_secret_access_key=self.key)
     
     def deliver(self, message):
         try:
-            destinations = [r.encode(encoding='utf-8') for r in message.recipients]
-            response = self.connection.send_raw_email(str(message), message.author.encode(), destinations)
-            
+            response = self.connection.send_raw_email(
+                    str(message),
+                    source=message.author.encode(),
+                    destinations=message.recipients.encode(),
+            )
             return (
                     response['SendRawEmailResponse']['SendRawEmailResult']['MessageId'],
                     response['SendRawEmailResponse']['ResponseMetadata']['RequestId']
